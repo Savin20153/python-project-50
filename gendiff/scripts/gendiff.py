25c7b2fd52ea3e8a0fd227074de131cfd5172d4b
Fedor Savin <Savin20153@gmail.com>
2025-04-03 21:20:55 +0300

    add -f

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,11 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    args = parser.parse_args()  
+    parser.add_argument(
+    '-f', '--format', default='stylish',
+    help='set format of output')
+    args = parser.parse_args()
+    
 
 if __name__ == '__main__':
     main()
