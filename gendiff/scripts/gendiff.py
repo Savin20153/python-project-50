7ddd8d24a792f9fc54ea059d168e1c678cc0926a
Fedor Savin <Savin20153@gmail.com>
2025-04-06 22:32:54 +0300

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,15 @@
-from gendiff import gen_diff
+from gendiff.get_files import get_files
+from gendiff.generate_diff import generate_diff
 
 
 def main():
-    gen_diff.generate_difference()
-    
+    first_file, second_file = get_files()
+    diff = generate_diff(first_file, second_file)
+    print('{')
+    for key, value in diff.items():
+        print(f'    {key}: {value}')
+    print('}')
+
 
 if __name__ == '__main__':
     main()
