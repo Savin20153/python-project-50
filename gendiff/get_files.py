2d3b7829efae1193ac3346ce36d6c6fc43139d5c
Fedor Savin <Savin20153@gmail.com>
2025-04-06 22:31:50 +0300

    add get_files.py

---
+++ gendiff/get_files.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+
+
+def get_files():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+    '-f', '--format', default='stylish',
+    help='set format of output')
+    args = parser.parse_args()
+
+    first_file = json.load(open(args.first_file))
+    second_file = json.load(open(args.second_file))
+
+    return [first_file, second_file]
+
+
+
+
+
+
+
