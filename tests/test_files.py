251a95b2e47066e919bbe2d42214be963745649d
Fedor Savin <Savin20153@gmail.com>
2025-04-20 21:57:25 +0300

    rename generator to gendiff

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,7 +1,7 @@
 import pytest
 import json
 import os
-from gendiff.generator import generate_diff
+from gendiff.gendiff import generate_diff
 
 def get_path(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
